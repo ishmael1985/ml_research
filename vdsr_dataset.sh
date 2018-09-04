440b23938e2c1e7ef409419185fe8b6db4d176c8
Ishmael Visayana Sameen <ishmael1985@gmail.com>
2018-09-04 20:45:38 +0200

    Modify script and config files to replicate original VDSR results

--- vdsr_dataset.sh
+++ vdsr_dataset.sh
@@ -6,23 +6,23 @@ python prepare_data.py --image_folder=$1 --sample_size=-1 --save_images --save_d
 downsize=( 1.4286 2 )
 for i in "${downsize[@]}"
 do
-	python prepare_data.py --image_folder=$1 --scale=$i --save_images --dataset_csv=dataset.csv
+	python prepare_data.py --image_folder=$1 --scale $i --save_grayscale --dataset_csv=dataset.csv
 done
 
 # Augment with rotated images of 90, 180 and 270
 python prepare_data.py --image_folder=generated --sample_size=-1 --save_dataset
 
-rotate=( 90 180 -90 )
+rotate=( 90 180 270 )
 for i in "${rotate[@]}"
 do
-	python prepare_data.py --image_folder=generated --rotate=$i --save_images --dataset_csv=dataset.csv
+	python prepare_data.py --image_folder=generated --rotate $i --save_grayscale --dataset_csv=dataset.csv
 done
 
 # Augment with flipped images
 python prepare_data.py --image_folder=generated --sample_size=-1 --save_dataset
 
-python prepare_data.py --image_folder=generated --flip_horizontal --save_images --dataset_csv=dataset.csv
-python prepare_data.py --image_folder=generated --flip_vertical --save_images --dataset_csv=dataset.csv
+python prepare_data.py --image_folder=generated --flip_horizontal --save_grayscale --dataset_csv=dataset.csv
+python prepare_data.py --image_folder=generated --flip_vertical --save_grayscale --dataset_csv=dataset.csv
 
 # Create hdf5 file for scale factors 2, 3, 4
 python prepare_data.py --image_folder generated --sample_size -1 --hdf5_path train.h5
